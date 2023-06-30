2d05d17a6a5de7fa45fcdb1a9607dcb6412ee276
sam <sam@mollybet.com>
2023-06-30 09:37:08 +0100

    fix: Add comments to run file

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 # Ensure we are on the latest
 
+printf "\n ---- Checking internet connection ---- \n"
 if hostname -I; then
+    printf "\n ---- Pulling master ---- \n"
     git fetch origin;
     git reset --hard origin/master;
 fi
 
 # Start tmux session with frontend and can running
 
+printf "\n ---- Starting tmux ---- \n"
 tmux new-session -d -s "pat" "cd can && . ./run.sh";
 tmux split-window -h;
 tmux send-keys "cd frontend && . ./run.sh" Enter;
