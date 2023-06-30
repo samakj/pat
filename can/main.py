c2a399d602302c9342e551478b13222a2dea6799
sam <sam@mollybet.com>
2023-06-30 11:06:34 +0100

    fix: Add can router to app

--- can/main.py
+++ can/main.py
@@ -3,10 +3,14 @@ from speedyapi import SpeedyAPI
 from speedyapi.websockets import WebsocketsStore
 from sniffer import CANSniffer
 
+from routes.can import CAN_V0_ROUTER
+
 app = SpeedyAPI()
 app.config = config
 app.websockets = WebsocketsStore()
 app.sniffer = CANSniffer(websockets=app.websockets, logger=app.logger)
+
+app.include_router(CAN_V0_ROUTER)
 
 
 @app.on_event("startup")  # type: ignore
