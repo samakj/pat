15489464dbe684151aa5ecef32f7831b9fe2571e
sam <sam@mollybet.com>
2023-06-30 09:12:54 +0100

    fix: Pull master on startup

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+# Ensure we are on the latest
+
+if hostname -I; then
+    git fetch origin;
+    git reset --hard origin/master;
+fi
+
+# Start tmux session with frontend and can running
+
 tmux new-session -d -s "pat" "cd can && . ./run.sh";
 tmux split-window -h;
 tmux send-keys "cd frontend && . ./run.sh" Enter;
