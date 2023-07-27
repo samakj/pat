9bb9b4c84b1a37c374c7891c8910489191b3d59b
sam <sam@mollybet.com>
2023-07-27 10:27:46 +0100

    fix: Add some data mappings

--- can/routes/can.py
+++ can/routes/can.py
@@ -6,7 +6,24 @@ from speedyapi.websockets import Websockets
 
 CAN_V0_ROUTER = APIRouter(prefix="/v0/can", tags=["can"])
 
-mappings = [DataMapping(name="test", arbitration_id=0, bits=(1, 2), format="binary")]
+mappings = [
+    DataMapping(name="handbrake", arbitration_id=1477, bits=(5, 6), format="bool"),
+    DataMapping(name="boot open", arbitration_id=1549, bits=(0, 1), format="bool"),
+    DataMapping(
+        name="drivers door open", arbitration_id=1549, bits=(3, 4), format="bool"
+    ),
+    DataMapping(
+        name="passenger door open", arbitration_id=1549, bits=(4, 5), format="bool"
+    ),
+    DataMapping(name="side lights on", arbitration_id=1549, bits=(5, 6), format="bool"),
+    DataMapping(name="head lights on", arbitration_id=1549, bits=(6, 7), format="bool"),
+    DataMapping(
+        name="left indicator on", arbitration_id=1549, bits=(10, 11), format="bool"
+    ),
+    DataMapping(
+        name="right indicator on", arbitration_id=1549, bits=(11, 12), format="bool"
+    ),
+]
 
 
 @CAN_V0_ROUTER.get("/mappings", response_model=list[DataMapping])
